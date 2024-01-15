abe5ccd24c22bb745494f30e64086865abba35d4
river <laolei@forkway.cn>
2024-01-15 18:18:56 +0800

    feat: rename repo

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,11 @@ const args = yargs(hideBin(process.argv))
   .command(deployCommand)
   .demandCommand(1, 'Please provide a command')
   .usage('$0 [command] [options]')
-  .epilogue('👻 Explore our manual on https://github.com/ConnectAI-E/botops-cli')
+  .epilogue('👻 Explore our manual on https://github.com/ConnectAI-E/botops')
   .example('$0 info', 'Check current authorization status')
   .example('$0 auth --feishu', 'Interactively log in and authorize with Feishu')
   .example('$0 deploy bot.json ', 'Deploy the bot to a specific platform using a local configuration file')
-  .example('$0 deploy https://example.bot.json ', 'Deploy the bot to a specific platform using a remote configuration URL')
+  .example('$0 deploy https://example/bot.json ', 'Deploy the bot to a specific platform using a remote configuration URL')
 
   .help()
   .argv
